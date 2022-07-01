dda60512df3f5cee943aac24d85660d8027b29f1
verdo-16 <kevinwasonga16@gmail.com>
2022-06-30 18:31:03 -0700

    bt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,22 +2,9 @@
 
 /**
  *  * main - Entry point
- *   * print word to output
+ *   *
  *    * Return: Always 0 (Success)
  *     */
-
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}#include <stdio.h>
-
-/**
- *  * main - Entry point
- *   * print word to output
- *    * Return: Always 0 (Success)
- *     */
-
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
