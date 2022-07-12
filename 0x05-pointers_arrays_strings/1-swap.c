065a2a7d064b459f0370911280a37d2528e72993
verdo-16 <kevinwasonga16@gmail.com>
2022-07-12 12:18:14 -0700

    fala

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+lude "main.h"
+
+/**
+ *  * swap_int - swap two integers
+ *   * @a: integer
+ *    * @b: integer
+ *     *
+ *      * Return: void
+ *       */
+void swap_int(int *a, int *b)
+{
+		int temp = *a;
+
+			*a = *b;
+				*b = temp;
+}
